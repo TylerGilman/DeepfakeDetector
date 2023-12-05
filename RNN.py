9f7ff8d68ed1509406bd8dfa3872179cca979ef9
Nicholas Soares <nsoares22@lasalle-academy.org>
2023-12-04 19:27:22 -0500

    Changed graphing code again to work

--- RNN.py
+++ RNN.py
@@ -232,7 +232,7 @@ if __name__ == "__main__":
             print(f"Accuracy of {classes[i]}: {acc:.2f} %")
 
     #Print the loss function over time
-    plt.plot(len(loss_values), loss_values)
+    plt.plot(range(len(loss_values)), loss_values)
     plt.title("Loss over iterations")
     plt.xlabel("Iterations")
     plt.ylabel("Loss")
